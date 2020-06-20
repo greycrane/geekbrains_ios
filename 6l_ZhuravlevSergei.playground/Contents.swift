92544165cbde98f9a23a57eaed1e3aebf546153c
greycrane <gcrane@bk.ru>
2020-06-20 12:36:00 +0300

    homework 6 done, try 2

--- 6l_ZhuravlevSergei.playground/Contents.swift
+++ 6l_ZhuravlevSergei.playground/Contents.swift
@@ -1,15 +1,15 @@
 import UIKit
 
-struct Stack<T> {
+struct Queue<T> {
     private var elements: [T] = []
     
-    mutating func push(_ element: T) {
+    mutating func enqueue(_ element: T) {
         elements.append(element)
     }
     
-    mutating func pop() -> T? {
+    mutating func dequeue() -> T? {
         guard elements.count > 0 else { return nil }
-        return elements.removeLast()
+        return elements.removeFirst()
     }
     
     func filter(predicate: (Int) -> Bool) -> [Int] {
@@ -20,14 +20,6 @@ struct Stack<T> {
             }
         }
         return tmpArray
-    }
-    
-    subscript(elements: Int ...) -> Bool? {
-        if elements.count < 1 {
-            return nil
-        } else {
-            return true
-        }
     }
 }
 
@@ -41,15 +33,15 @@ var greaterThanZero: (Int) -> Bool = { (element: Int) -> Bool in
     return element > 0
 }
 
-var intsArray = Stack<Int>()
-intsArray.pop()
-intsArray.push(20)
-intsArray.push(13)
-intsArray.push(10)
-intsArray.pop()
-intsArray.push(-4)
-intsArray.push(1217)
-intsArray.push(134)
+var intsArray = Queue<Int>()
+intsArray.dequeue()
+intsArray.enqueue(20)
+intsArray.enqueue(13)
+intsArray.enqueue(10)
+intsArray.dequeue()
+intsArray.enqueue(-4)
+intsArray.enqueue(1217)
+intsArray.enqueue(134)
 print(intsArray)
 print(intsArray.filter(predicate: evenNumber))
 
